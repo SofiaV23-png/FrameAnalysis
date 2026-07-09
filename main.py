d852a5342e4263ff2bad57cccdf7705443d673e7
Eva Torres Gonzalez <eva.torresgonzalez@icloud.com>
2026-07-09 11:31:22 +0100

    Length method for Frame Element class created

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from models.node import Node
 from models.frameelement import FrameElement
 
-node_1 = Node(1, 0, 0)
-node_2 = Node(1, 1, 0)
+node_1 = Node(1, 1, 1)
+node_2 = Node(2, 0, 0)
 
 beam = FrameElement(node_1, node_2, 210, 100, 10e8)
-print(beam.start)
+print(beam.length())
--- models/frameelement.py
+++ models/frameelement.py
@@ -1,7 +1,15 @@
+import numpy as np
+
 class FrameElement:
     def __init__(self, start, end, E, A, I):
         self.start = start
         self.end = end
+        # Units are GPa for E, mm^2 for A, mm^4 for I
         self.E = E
         self.A = A
         self.I = I
+
+    def length(self):
+        length = np.sqrt((self.start.x-self.end.x)**2 + (self.start.y-self.end.y)**2)
+        return length
+    
