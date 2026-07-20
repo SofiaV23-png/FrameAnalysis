a60a0bd31d2af0dd778557fd9b9a183b181fc762
Eva Torres Gonzalez <eva.torresgonzalez@icloud.com>
2026-07-20 18:24:18 +0100

    Implementation of local stiffness matrix and transformation matrix

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ node_1 = Node(1, 1, 1)
 node_2 = Node(2, 0, 0)
 
 beam = FrameElement(node_1, node_2, 210, 100, 10e8)
-print(beam.length())
+print(beam.angle())
+
+value = 2
+print(-value)
--- models/frameelement.py
+++ models/frameelement.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class FrameElement:
     def __init__(self, start, end, E, A, I):
@@ -10,6 +11,32 @@ class FrameElement:
         self.I = I
 
     def length(self):
-        length = np.sqrt((self.start.x-self.end.x)**2 + (self.start.y-self.end.y)**2)
+        length = np.sqrt((self.end.x-self.start.x)**2 + (self.end.y-self.start.y)**2)
         return length
     
+    def angle(self):
+        x_dif = self.end.x-self.start.x
+        y_dif = self.end.y-self.start.y
+        angle = math.atan2(y_dif, x_dif)
+        return angle
+    
+    def local_stiffness(self):
+        # in elemental coordinate system
+        local_stiffness_matrix = np.array([[self.A, 0, 0, -self.A, 0, 0],
+                                  [0, 12*self.I/(self.L)**2, 6*self.I/self.L, 0, -12*self.I/(self.L)**2, 6*self.I/self.L],
+                                  [0, 6*self.I/self.L, 4*self.I, 0, -6*self.I/self.L, 2*self.I],
+                                  [-self.A, 0, 0, self.A, 0, 0],
+                                  [0, -12*self.I/(self.L)**2, -6*self.I/self.L, 0, 12*self.I/(self.L)**2, -6*self.I/self.L],
+                                  [0, 6*self.I/self.L, 2*self.I, 0, -6*self.I/self.L, 4*self.I]])
+        return local_stiffness_matrix
+    
+    def translation(self):
+        translation_matrix = np.array([[np.cos(self.angle), np.sin(self.angle), 0, 0, 0, 0],
+                              [-np.sin(self.angle), np.cos(self.angle), 0, 0, 0, 0],
+                              [0, 0, 1, 0, 0, 0],
+                              [0, 0, 0, np.cos(self.angle), np.sin(self.angle), 0],
+                              [0, 0, 0, np.sin(self.angle), np.cos(self.angle), 0],
+                              [0, 0, 0, 0, 0, 1]])
+        return translation_matrix
+    
+
