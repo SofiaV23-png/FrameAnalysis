4fe525fe9f47d4dd15f5c2d5e54b938e0784489d
Eva Torres Gonzalez <eva.torresgonzalez@icloud.com>
2026-08-03 20:25:21 +0100

    Implementing global matrix method

---
+++ analysis/assembler.py
@@ -0,0 +1,21 @@
+# obtain elemental stiffness matrix in global CSYS
+# assemble global stiffness matrix
+import numpy as np
+
+class Assembler:
+    def __init__(self, frame):
+        self.frame = frame
+
+    def assemble_stiffness():
+        n_dof = len(self.frame.nodes) * 3
+        K = np.zeros((n_dof, n_dof))
+
+        for element in self.frame.elements:
+            k = element.global_stiffness()
+            dofs = element.map_dof()
+            K[np.ix_(dofs, dofs)] += k
+
+        return K
+
+    def assemble_loads():
+        pass
